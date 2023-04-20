ea2d2589d8d9fb0b89d70fd5aafffae2d9290571
Alexander Dimov <alex.lemuel@gmail.com>
2023-04-20 20:33:25 +0300

    added function for displaying images

--- Single page application - Aria web-page/script.js
+++ Single page application - Aria web-page/script.js
@@ -6,20 +6,15 @@ const cities = {
         './images/car.jpg', './images/drums.jpg', './images/eleven.jpg', './images/kitchen.jpg',
         './images/klasa.jpg', './images/koncert-na-aria.jpg', './images/portrait_aria_and_me.jpg',
         './images/singing.jpg', './images/sofia.jpg', './images/trio bik and aria.jpg',
-        './images/varna.jpg', './images/plovdiv.jpg'],
+        './images/varna.jpg'],
 
-    'софия': ['./images/sofia.jpg', './images/singing.jpg', './images/drums.jpg', './images/eleven.jpg'],
-    'пловдив': ['./images/plovdiv.jpg', './images/black-white.jpg', './images/bushido.jpg', './images/klasa.jpg'],
-    'варна': ['./images/varna.jpg', './images/car.jpg', './images/300.jpg', './images/eleven.jpg'],
-    'стара загора': ['./images/portrait_aria_and_me.jpg', './images/trio bik and aria.jpg', './images/aria and me.jpg', './images/stara zagora.jpg']
+    'софия': ['./images/sofia.jpg', './images/singing.jpg', './images/drums.jpg'],
+    'пловдив': ['./images/plovdiv.jpg', './images/black-white.jpg', './images/bushido.jpg'],
+    'варна': ['./images/varna.jpg', './images/car.jpg', './images/300.jpg'],
+    'стара загора': ['./images/portrait_aria_and_me.jpg', './images/trio bik and aria.jpg', './images/aria and me.jpg']
 }
 
-cities.всички.forEach(path => {
-    const img = document.createElement('img');
-    img.src = path;
-
-    boxContainer.appendChild(img);
-});
+displayImages('всички');
 
 for (const btn of allBtns) {
     btn.addEventListener('click', (e) => {
@@ -29,15 +24,25 @@ for (const btn of allBtns) {
             e.target.classList.add('active');
             currentActiveBtn[0].classList.remove('active');
 
-            boxContainer.innerHTML = '';
-            cities[btn.textContent].forEach(path => {
-                const img = document.createElement('img');
-                img.src = path;
-
-                boxContainer.appendChild(img);
-            });
+            displayImages(btn.textContent);
         }
     })
+}
+
+function displayImages(location) {
+    boxContainer.innerHTML = '';
+
+    cities[location].forEach(path => {
+        const anchorTag = document.createElement('a');
+        anchorTag.href = path;
+
+        const img = document.createElement('img');
+        img.src = path;
+
+        anchorTag.appendChild(img);
+
+        boxContainer.appendChild(anchorTag);
+    });
 }
 
 
